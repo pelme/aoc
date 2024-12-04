f24679b8245c1580780e492a874b3208fac61868
Andreas Pelme <andreas@pelme.se>
2024-12-04 14:50:50 +0100

    Day 2, part 1 working.

--- 2024/day-2.nix
+++ 2024/day-2.nix
@@ -1,18 +1,52 @@
 { nixpkgs, file }:
 let
-  input = s.trim (b.readFile file);
-  b = builtins;
-  s = nixpkgs.lib.strings;
+  pipe = nixpkgs.lib.trivial.pipe;
+  trim = nixpkgs.lib.strings.trim;
+  readFile = builtins.readFile;
+  splitString = nixpkgs.lib.strings.splitString;
+  toInt = nixpkgs.lib.strings.toInt;
+  head = builtins.head;
+  tail = builtins.tail;
+  length = builtins.length;
+  concatLists = builtins.concatLists;
+  filter = builtins.filter;
+  sort = builtins.sort;
+  all = builtins.all;
 
-  rawLines = s.splitString "\n" input;
-  parseLine = line: map s.toInt (s.splitString " " line);
-  lines = map parseLine rawLines;
-  isSortedBy = cmp: line: (b.sort cmp line) == line;
+  isSortedBy = cmp: line: (sort cmp line) == line;
   isIncrOrDecr = line: (isSortedBy (x: y: x < y) line || isSortedBy (x: y: x > y) line);
+  abs = x: if x > 0 then x else -x;
+  isGoodDiff =
+    pair:
+    let
+      diff = (abs ((head pair) - (head (tail pair))));
+    in
+    diff > 0 && diff <= 3;
+
+  pairs =
+    xs:
+    (
+      if (length xs) < 2 then
+        [ ]
+      else
+        (concatLists [
+          [
+            [
+              (head xs)
+              (head (tail xs))
+            ]
+          ]
+          (pairs (tail xs))
+        ])
+    );
 in
-{
-  # part1 = input;
-  # out = map parseLine rawLines;
-  out = lines;
-  goodie = map isIncrOrDecr lines;
-}
+(pipe file [
+  readFile
+  trim
+  (splitString "\n")
+  (map (line: map toInt (splitString " " line)))
+  (filter isIncrOrDecr)
+  (map pairs)
+  (filter (all isGoodDiff))
+  length
+])
